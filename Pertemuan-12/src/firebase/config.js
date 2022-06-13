3fc9c71918552d310e191f281ac4175d7a907a43
FarhanAgil <drakek1977@gmail.com>
2022-06-13 20:23:43 +0700

    commit js 12

---
+++ Pertemuan-12/src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYOBTg-0sQZ64VwaShOirLuI-DTAbhztI",
+  authDomain: "pertemuan-12-1a1e8.firebaseapp.com",
+  projectId: "pertemuan-12-1a1e8",
+  storageBucket: "pertemuan-12-1a1e8.appspot.com",
+  messagingSenderId: "671623837414",
+  appId: "1:671623837414:web:8dc169c0943fd219a9ed27"
+};
+export default firebaseConfig;
