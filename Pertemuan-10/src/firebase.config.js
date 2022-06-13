d33091aff84357c22fa1fd4448363e9b6404a30b
FarhanAgil <drakek1977@gmail.com>
2022-06-13 19:18:04 +0700

    commit js 10

---
+++ Pertemuan-10/src/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+      apiKey: "AIzaSyCxPQDwWj6nqj8lYURNPRcN-D05KX-l4k0",
+      authDomain: "pertemuan-10-c518a.firebaseapp.com",
+      projectId: "pertemuan-10-c518a",
+      storageBucket: "pertemuan-10-c518a.appspot.com",
+      messagingSenderId: "900320516890",
+      appId: "1:900320516890:web:2eb6032a49d6b10e986aa3"
+}; 
